116016c6e2a653ec2c25d63f3e3836f225d039e8
ead <ead@academico.local>
2025-06-13 20:38:13 -0300

    aula de repetição

--- atividades/atividade_01/main.py
+++ atividades/atividade_01/main.py
@@ -5,21 +5,30 @@ O usuario pode repetir várias vezes e encerrar o programa quando desejar.
 calculo: compensa etanol caso ele tenha até 70% do valor da gasolina. - seja 30% mais barato que a gasolina
 
 """
+# Laço de Repetição - while True
 
 while True:
-    print(f"{'-'*20} Comparar Preço de Combustiveis {'-'*20} \n")
-    print("0 - para SAIR")
-    print("1 - para CONTINUAR")
+    
+        # Tratamento de Exceção - try / except
+    try:
+        gas = float(input("Informe valor da Gasolina: ").replace(",", "."))
+        eta = float(input("Informe valor do Etanol: ").replace(",", "."))
+        calc = eta/gas*100
 
-    operador = input("Continuar ou Sair: ").strip()   
+        result = "Gasolina" if calc > 70 else "Etanol"      # ternario, opção simples para esolha sim ou não
 
-    gas = float(input("Informe valor da Gasolina: ").replace(",", "."))
-    eta = float(input("Informe valor do Etanol: ").replace(",", "."))
+        print(f"Calculo {calc:.2f} % da Gasolina")    # :. 2f  casas decimais no numero
+        print(f"Compensa abastecer com {result}")
 
-    gas_calc = gas * 0.7
-    result = eta - gas_calc
-
-    if eta < gas_calc:
-        print(f" Etanol (30%) {result} a menos que a Gasolina -  VALE A PENA ABASTERNCER COM ETANOL.")
-    else:
-        print(f"Melhor abastecer com Gasolina.")
+        opcao = input("Novo calculo? S / N : ").lower().strip()    # louwer para digitos minusculos ou maisculos / strip para caracter invalido
+        match opcao:        # match e case , usa para varias opções
+            case "s":
+                continue
+            case "n":
+                break
+            case _:
+                print("Opção invalida")
+                continue
+    except Exception as e:
+        print(f"Não foi possivel executar a operação.{e}")    
+        continue
---
+++ atividades/atividade_02/main.py
@@ -0,0 +1,58 @@
+"""
+Programa que recebe Nome, Peso e Altura e calcule o IMC.
+mostrar o valor do IMC em duas casas decimais, e mostrar o diagnostico:
+
+caso o valor seja menor que 18.5 = abaixo do pelo
+caso o valor seja maior ou igual 18.5 e menor que 25 = peso ideal
+caso o valor seja maior ou igual 25 e menor que 30 = acima do peso
+caso o valor seja maior ou igual 30 e menor que 35 = obeso
+caso o valor seja maior ou igual 35 e menor que 40 = obesidade nivel-2
+caso o valor seja maior ou igual a 40 = obesidade morbita
+
+o usuario pode repetir o calculo quantas vezes quiser, ou encerrar o programa
+"""
+
+while True:
+    
+        # Tratamento de Exceção - try / except
+    try:
+        nome = input("informe seu nome: ")
+        peso = float(input("Informe seu Peso: ").replace(",", "."))
+        alt = float(input("Informe sua Altura: ").replace(",", "."))
+     
+        imc = peso/(alt**2)   
+          
+        if imc <=18.5:
+            print(f"o valor do IMC é: {imc:.0f} - Voce esta ABAIXO do peso")    # :. 2f  casas decimais no numero
+
+        elif imc >=18.5 and imc <25:
+            print(f"o valor do IMC é: {imc:.0f} - Voce esta no peso IDEAL")
+
+        elif imc >=25 and imc <30:
+            print(f"o valor do IMC é: {imc:.0f} - Voce esta ACIMA do peso")
+
+        elif imc >=30 and imc <35:
+            print(f"o valor do IMC é: {imc:.0f} - Voce esta OBESO")
+
+        elif imc >=35 and imc <40:
+            print(f"o valor do IMC é: {imc:.0f} - Voce esta OBESO - Nivel 2")
+
+        elif imc >=40:
+            print(f"o valor do IMC é: {imc:.0f} - Voce esta com OBESIDADE MORBITA")
+
+        else:
+            print("ACIMA")    
+
+
+        opcao = input("Novo calculo? S / N : ").lower().strip()    # louwer para digitos minusculos ou maisculos / strip para caracter invalido
+        match opcao:        # match e case , usa para varias opções
+            case "s":
+                continue
+            case "n":
+                break
+            case _:
+                print("Opção invalida")
+                continue
+    except Exception as e:
+        print(f"Não foi possivel executar a operação.{e}")    
+        continue
