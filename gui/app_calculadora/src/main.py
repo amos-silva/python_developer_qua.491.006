c2b6df01e85417cc4a67f19424a53d66b0e07d54
amos-silva <amosdasilva86@gmail.com>
2025-09-08 21:38:58 -0300

    App Calculadora - flet

---
+++ gui/app_calculadora/src/main.py
@@ -0,0 +1,194 @@
+import flet as ft
+
+
+class CalcButton(ft.ElevatedButton):
+    def __init__(self, text, button_clicked, expand=1):
+        super().__init__()
+        self.text = text
+        self.expand = expand
+        self.on_click = button_clicked
+        self.data = text
+
+
+class DigitButton(CalcButton):
+    def __init__(self, text, button_clicked, expand=1):
+        CalcButton.__init__(self, text, button_clicked, expand)
+        self.bgcolor = ft.Colors.WHITE24
+        self.color = ft.Colors.WHITE
+
+
+class ActionButton(CalcButton):
+    def __init__(self, text, button_clicked):
+        CalcButton.__init__(self, text, button_clicked)
+        self.bgcolor = ft.Colors.ORANGE
+        self.color = ft.Colors.WHITE
+
+
+class ExtraActionButton(CalcButton):
+    def __init__(self, text, button_clicked):
+        CalcButton.__init__(self, text, button_clicked)
+        self.bgcolor = ft.Colors.BLUE_GREY_100
+        self.color = ft.Colors.BLACK
+
+
+class CalculatorApp(ft.Container):
+
+    def __init__(self):
+        super().__init__()
+        self.reset()
+
+        self.result = ft.Text(value="0", color=ft.Colors.WHITE, size=20)
+        self.width = 350
+        self.bgcolor = ft.Colors.BLACK
+        self.border_radius = ft.border_radius.all(20)
+        self.padding = 20
+        self.content = ft.Column(
+            controls=[
+                ft.Row(controls=[self.result], alignment="end"),
+                ft.Row(
+                    controls=[
+                        ExtraActionButton(
+                            text="AC", button_clicked=self.button_clicked
+                        ),
+                        ExtraActionButton(
+                            text="+/-", button_clicked=self.button_clicked
+                        ),
+                        ExtraActionButton(text="%", button_clicked=self.button_clicked),
+                        ActionButton(text="/", button_clicked=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(text="7", button_clicked=self.button_clicked),
+                        DigitButton(text="8", button_clicked=self.button_clicked),
+                        DigitButton(text="9", button_clicked=self.button_clicked),
+                        ActionButton(text="*", button_clicked=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(text="4", button_clicked=self.button_clicked),
+                        DigitButton(text="5", button_clicked=self.button_clicked),
+                        DigitButton(text="6", button_clicked=self.button_clicked),
+                        ActionButton(text="-", button_clicked=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(text="1", button_clicked=self.button_clicked),
+                        DigitButton(text="2", button_clicked=self.button_clicked),
+                        DigitButton(text="3", button_clicked=self.button_clicked),
+                        ActionButton(text="+", button_clicked=self.button_clicked),
+                    ]
+                ),
+                ft.Row(
+                    controls=[
+                        DigitButton(
+                            text="0", expand=2, button_clicked=self.button_clicked
+                        ),
+                        DigitButton(text=".", button_clicked=self.button_clicked),
+                        ActionButton(text="=", button_clicked=self.button_clicked),
+                    ]
+                ),
+            ]
+        )
+
+    def button_clicked(self, e):
+        data = e.control.data
+        print(f"Button clicked with data = {data}")
+        if self.result.value == "Error" or data == "AC":
+            self.result.value = "0"
+            self.reset()
+
+        elif data in ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."):
+            if data == "." and "." in self.result.value:
+             
+                return
+            if self.result.value == "0" or self.new_operand == True:
+                self.result.value = data
+                self.new_operand = False
+            else:
+                self.result.value = self.result.value + data
+
+        elif data in ("+", "-", "*", "/"):
+            
+            if self.new_operand:
+                self.operator = data
+            else:
+                try:
+                    res = self.calculate(self.operand1, float(self.result.value), self.operator)
+                    self.result.value = str(res)
+                    if res == "Error":
+                        self.operand1 = 0
+                    else:
+                        self.operand1 = float(res)
+                    self.operator = data
+                except Exception:
+                    self.result.value = "Error"
+                    self.operand1 = 0
+            self.new_operand = True
+
+        elif data == "=":
+            try:
+                res = self.calculate(self.operand1, float(self.result.value), self.operator)
+                self.result.value = str(res)
+            except Exception:
+                self.result.value = "Error"
+            self.reset()
+
+        elif data == "%":
+            try:
+                self.result.value = str(float(self.result.value) / 100)
+            except Exception:
+                self.result.value = "Error"
+            self.reset()
+
+        elif data == "+/-":
+            try:
+                current_value = float(self.result.value)
+                current_value = -current_value
+                self.result.value = str(self.format_number(current_value))
+            except Exception:
+                self.result.value = "Error"
+
+        self.update()
+
+
+    def format_number(self, num):
+        if num % 1 == 0:
+            return int(num)
+        else:
+            return num
+
+    def calculate(self, operand1, operand2, operator):
+
+        if operator == "+":
+            return self.format_number(operand1 + operand2)
+
+        elif operator == "-":
+            return self.format_number(operand1 - operand2)
+
+        elif operator == "*":
+            return self.format_number(operand1 * operand2)
+
+        elif operator == "/":
+            if operand2 == 0:
+                return "Error"
+            else:
+                return self.format_number(operand1 / operand2)
+
+    def reset(self):
+        self.operator = "+"
+        self.operand1 = 0
+        self.new_operand = True
+
+
+def main(page: ft.Page):
+    page.title = "Calculadora"
+    
+    calc = CalculatorApp()
+    
+    page.add(calc)
+
+
+ft.app(target=main)
