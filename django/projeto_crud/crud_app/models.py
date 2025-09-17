6de2913bf33f8084e547a7540596940ae7cf0957
amos-silva <amosdasilva86@gmail.com>
2025-09-16 21:45:33 -0300

    Django - Preparao do Projeto Crud

--- django/projeto_crud/crud_app/models.py
+++ django/projeto_crud/crud_app/models.py
@@ -5,5 +5,8 @@ class Pessoa(models.Model):
     id_pessoa = models.AutoField(primary_key=True)  # auto incremento
     nome = models.CharField(max_length=255, null=False, blank=False) # Campo obrigatorio, não vázio
     email = models.EmailField(unique=True, null=False, blank=False)  # Campo unico - não repetido e não vazio
-    altura = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    data_nasc = models.DateField(null=False, blank=False)
+    cpf = models.CharField(max_length=14,unique=True,null=True, blank=True)
+
+    # server para mostrar o nome correto na pagina de admin, ao invéz de mostrar o nome da classe
+    def __str__(self):
+        return self.nome
--- django/projeto_crud/crud_app/views.py
+++ django/projeto_crud/crud_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Olá Mundo Django , Estamos iniciando uma nova estapa, logo logo estaremos nas Nuvens !!!")
