43002076af469f0442214114180bee6db63a7828
amos-silva <amosdasilva86@gmail.com>
2025-09-17 21:41:45 -0300

    Projeto CRUD - Django

--- django/projeto_crud/crud_app/views.py
+++ django/projeto_crud/crud_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Pessoa
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Olá Mundo Django , Estamos iniciando uma nova estapa, logo logo estaremos nas Nuvens !!!")
+    pessoas = Pessoa.objects.all()
+    return render(request, "home.html", {"pessoas":pessoas})
+
