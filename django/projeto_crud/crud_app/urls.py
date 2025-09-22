31d82291ed9b79f249a4a52d181567e0386d6854
amos-silva <amosdasilva86@gmail.com>
2025-09-22 20:26:16 -0300

    alterar / excluir

--- django/projeto_crud/crud_app/urls.py
+++ django/projeto_crud/crud_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('cadastro/', views.cadastro_pessoa, name='cadastro_pessoa')
+    path('cadastro/', views.cadastro_pessoa, name='cadastro_pessoa'),
+    path('alterar/<int:id_pessoa>/', views.alterar_pessoa, name='alterar_pessoa'),
+    path('alterar/<int:id_pessoa>/', views.deletar_pessoa, name='deletar_pessoa')
 ]
